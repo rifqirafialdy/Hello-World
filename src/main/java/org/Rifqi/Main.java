91ef929816d20fe95926b26a8004cf4aab3024f7
rifqirafialdy <rifqirafialdy@gmail.com>
2025-02-04 00:38:18 +0700

    TES

--- src/main/java/org/Rifqi/Main.java
+++ src/main/java/org/Rifqi/Main.java
@@ -10,30 +10,29 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         //Rectangle
-        Rectangle rectangle= new Rectangle(5,3);
+        Rectangle rectangle = new Rectangle(5, 3);
         double rectangleArea = rectangle.getArea();
-        System.out.printf("Area of Rectangle %.1f\n",rectangleArea);
+        System.out.printf("Area of Rectangle %.1f\n", rectangleArea);
         //Circle
         Circle circle = new Circle(5);
-        System.out.printf("Circle Diameter : %.1f\n",circle.getDiameter());
-        System.out.printf("Circle Area : %.3f\n",circle.getArea());
-        System.out.printf("Circle Circumference : %.3f\n",circle.getCircumference());
+        System.out.printf("Circle Diameter : %.1f\n", circle.getDiameter());
+        System.out.printf("Circle Area : %.3f\n", circle.getArea());
+        System.out.printf("Circle Circumference : %.3f\n", circle.getCircumference());
         //Triangle
-        Triangle triangle= new Triangle(80,65);
-        System.out.printf("Missing Angle = %.1f\n",triangle.getMissingAngle());
+        Triangle triangle = new Triangle(80, 65);
+        System.out.printf("Missing Angle = %.1f\n", triangle.getMissingAngle());
         //Date
-        Date date= new Date(LocalDate.of(2024,03,19),LocalDate.of(2024,03,21));
-        System.out.println("Difference of date is : "+date.calculateDate() +" Days");
+        Date date = new Date(LocalDate.of(2024, 04, 19), LocalDate.of(2024, 03, 21));
+        System.out.println("Difference of date is : " + date.calculateDate() + " Days");
         //Initial
-        Initial initial= new Initial("John Doe");
+        Initial initial = new Initial("john Doe");
         char[] initials = initial.getInitial();
         System.out.println("Initials : " + new String(initials));
 
 
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
+        System.out.print("Hello and welcome!");
 
 
     }
